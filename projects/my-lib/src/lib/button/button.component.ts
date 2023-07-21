feb8a763ba059963af022346c628e654c843ff22
Khaled <khalegjr@gmail.com>
2023-07-21 15:24:07 -0300

    implementa o botão

--- projects/my-lib/src/lib/button/button.component.ts
+++ projects/my-lib/src/lib/button/button.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
-  selector: 'my-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  selector: "my-button",
+  templateUrl: "./button.component.html",
+  styleUrls: ["./button.component.css"],
 })
 export class ButtonComponent {
+  @Input() text: string = "Button";
+  @Output() click: EventEmitter<void> = new EventEmitter();
 
+  onclick() {
+    this.click.emit();
+  }
 }
