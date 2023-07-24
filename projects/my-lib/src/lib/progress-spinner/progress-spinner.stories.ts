392e424f415a43245ee8947f6c621c3436b24430
Khaled <khalegjr@gmail.com>
2023-07-23 22:57:29 -0300

    implementa componente progress-spinner

---
+++ projects/my-lib/src/lib/progress-spinner/progress-spinner.stories.ts
@@ -0,0 +1,34 @@
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { Meta, moduleMetadata, StoryObj } from "@storybook/angular";
+import { progressSpinnerArgtypes } from "./progress-spinner.argtypes";
+export default {
+  title: "components/Progress spinner",
+  decorators: [
+    moduleMetadata({
+      imports: [MatProgressSpinnerModule],
+    }),
+  ],
+  argTypes: progressSpinnerArgtypes,
+  parameters: {
+    controls: {
+      expanded: true,
+      exclude: ["mode"],
+    },
+  },
+} as Meta;
+
+export const WithBasicUsage: StoryObj = {
+  render: (args) => ({
+    props: args,
+    template: `
+    <style>
+        mat-progress-spinner:first-child {
+        margin-block-end: 3rem;
+        }
+    </style>
+    <mat-progress-spinner [color]="color" mode="determinate" [value]="value" [diameter]="diameter" [strokeWidth]="strokeWidth"></mat-progress-spinner>
+    <mat-progress-spinner [color]="color" mode="indeterminate" [value]="value" [diameter]="diameter" [strokeWidth]="strokeWidth"></mat-progress-spinner>
+`,
+  }),
+  name: "basic usage",
+};
