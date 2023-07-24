9961a79585b73b23b8fb81c66b4fa57c6a0e6011
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-07-24 17:10:21 -0300

    documentação mdx

--- projects/my-lib/src/lib/my-lib.module.ts
+++ projects/my-lib/src/lib/my-lib.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
-import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { NgModule } from '@angular/core';
 
-import { ButtonComponent } from "./button/button.component";
-import { MyLibComponent } from "./my-lib.component";
-import { ProgressBarComponent } from "./progress-bar/progress-bar.component";
+import { ButtonComponent } from './button/button.component';
+import { MyLibComponent } from './my-lib.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [MyLibComponent, ButtonComponent, ProgressBarComponent],
-  imports: [MatButtonModule, MatProgressBarModule],
+  imports: [],
   exports: [MyLibComponent],
 })
 export class MyLibModule {}
